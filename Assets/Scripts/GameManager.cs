0c6d758e5026008edf35961416b89025c3deff88
Emanuele Torelli <emanueletorelli@outlook.com>
2020-11-23 19:39:57 +0100

    selection

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.XR.Interaction.Toolkit.AR;
 
 public class GameManager : MonoBehaviour
 {
@@ -34,15 +35,31 @@ public class GameManager : MonoBehaviour
         List<GameObject> interactables = await Addressables.LoadAssetsAsync<GameObject>("Interactables", null).Task as List<GameObject>;
         for (int i = 0; i < 5; i++)
         {
-            Vector3 position = VirtualAssistantManager.Instance.transform.TransformPoint(0, 0, 0.2f) + new Vector3((float)Math.Pow(-1, i) * 0.4f * (i / 2), 0f, 0f);
-            Interactables.Add(Instantiate(interactables[i], position, interactables[i].transform.rotation));
+            Vector3 position = VirtualAssistantManager.Instance.transform.TransformPoint(0, 0, 0.15f) + new Vector3((float)Math.Pow(-1, i) * 0.4f * (i / 2), 0f, 0f);
+            GameObject interactable = Instantiate(interactables[i], position, interactables[i].transform.rotation);
+            
+            ARSelectionInteractable arSelectionInteractable = interactable.AddComponent<ARSelectionInteractable>();
+            GameObject selection = Instantiate(Resources.Load<GameObject>("Selection"), interactable.transform);
+            Bounds interactableBounds = interactable.GetComponent<Collider>().bounds;
+            selection.transform.position = new Vector3(selection.transform.position.x, selection.transform.position.y - interactableBounds.extents.y, selection.transform.position.z);
+            float radius = interactableBounds.extents.x > interactableBounds.extents.z ? interactableBounds.extents.x * 3 : interactableBounds.extents.z * 3;
+            selection.transform.localScale = new Vector3(radius, selection.transform.localScale.y, radius);
+            selection.SetActive(false);
+            arSelectionInteractable.selectionVisualization = selection;
+            
+            ARTranslationInteractable arTranslationInteractable = interactable.AddComponent<ARTranslationInteractable>();
+            arTranslationInteractable.objectGestureTranslationMode = GestureTransformationUtility.GestureTranslationMode.Any;
+            arTranslationInteractable.maxTranslationDistance = 10;
+            Interactables.Add(interactable);
         }
 
         List<GameObject> targets = await Addressables.LoadAssetsAsync<GameObject>("Targets", null).Task as List<GameObject>;
         for (int i = 0; i < targets.Count; i++)
         {
-            Vector3 position = VirtualAssistantManager.Instance.transform.TransformPoint(0, 0, -0.2f) + new Vector3((float)Math.Pow(-1, i) * 0.4f * (i / 2), 0f, 0f);
-            Interactables.Add(Instantiate(targets[i], position, targets[i].transform.rotation));
+            Vector3 position = VirtualAssistantManager.Instance.transform.TransformPoint(0, 0, 0.15f) + new Vector3((float)Math.Pow(-1, i) * 0.4f * (i / 2), 0f, 0f);
+            GameObject target = Instantiate(targets[i], position, targets[i].transform.rotation);
+            target.AddComponent<CollisionManager>();
+            Targets.Add(target);
         }
 
         Destroy(ARPlacementInteractable);
--- Assets/Scripts/StateMachine/IdleState.cs
+++ Assets/Scripts/StateMachine/IdleState.cs
@@ -7,8 +7,8 @@ public class IdleState : StateMachineBehaviour {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        VirtualAssistantManager.Instance.GetComponent<Animator>().ResetTrigger("Walk");
-        VirtualAssistantManager.Instance.SetTriggers();
+        //VirtualAssistantManager.Instance.GetComponent<Animator>().ResetTrigger("Walk");
+        //VirtualAssistantManager.Instance.SetTriggers();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
